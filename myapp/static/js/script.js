d4358fee5a7f1d636cf2c0c65293e5e93231a772
deltaplanoo <pietro.ciani01@gmail.com>
2023-11-29 18:53:56 +0100

    table switch theme according to global theme

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -1,8 +1,31 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
-themeToggle.addEventListener('click', function() {
+// Conditionally format rows
+function condFormat() {
+  var rows = document.querySelectorAll('table tr');
+  rows.forEach(function(row) {
+    if (row.cells[0].textContent === 'TBC') {
+      row.classList.add('table-warning');
+    }else if (row.cells[0].textContent === 'Go') {
+      row.classList.add('table-success');
+    }
+  });
+}
+
+themeToggle.addEventListener('click', function () {
+  // Toggle dark mode for the body
   body.classList.toggle('dark-mode');
+
+  // Toggle dark mode for every table
+  const tables = document.querySelectorAll('table');
+  tables.forEach(table => {
+    table.classList.toggle('table-dark');
+  });
+
+  // Update the theme toggle button icon
   const isDarkMode = body.classList.contains('dark-mode');
   themeToggle.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
 });
+
+document.addEventListener('DOMContentLoaded', condFormat);

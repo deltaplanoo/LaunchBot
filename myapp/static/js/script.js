746c4c1323ff23951157adc49201688faa626d1c
pietro.ciani01@gmail.com <pietro.ciani01@gmail.com>
2023-11-30 12:05:55 +0100

    dark theme finally working

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -1,19 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const body = document.body;
-  const themeToggle = document.getElementById('themeToggle');
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
 
-  function themeSwitch() {
-    body.classList.toggle('dark-mode');
-
-    const tables = document.querySelectorAll('table');
-    tables.forEach(table => {
-      table.classList.toggle('table-dark');
-    });
-
-    const isDarkMode = body.classList.contains('dark-mode');
-    themeToggle.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
-  }
-
-  // Add event listener for themeToggle button
-  themeToggle.addEventListener('click', themeSwitch);
+themeToggle.addEventListener('click', function() {
+  body.classList.toggle('dark-mode');
+  const isDarkMode = body.classList.contains('dark-mode');
+  themeToggle.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
 });

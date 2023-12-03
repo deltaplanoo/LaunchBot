5dc7ac15cd91f6b1d6e2c5ca7f4572a3e9affd57
pietro.ciani01@gmail.com <pietro.ciani01@gmail.com>
2023-12-03 12:49:37 +0100

    first try implementing theme local storage

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -1,8 +1,30 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
-themeToggle.addEventListener('click', function() {
-  body.classList.toggle('dark-mode');
+// Function to set the theme
+function setTheme(theme) {
+  body.classList.toggle('dark-mode', theme === 'dark');
   const isDarkMode = body.classList.contains('dark-mode');
   themeToggle.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+
+  // Save the theme preference in localStorage
+  localStorage.setItem('theme', theme);
+}
+
+// Add click event listener to toggle theme
+themeToggle.addEventListener('click', function() {
+  const isDarkMode = body.classList.contains('dark-mode');
+  setTheme(isDarkMode ? 'light' : 'dark');
+});
+
+// Page initialization
+document.addEventListener('DOMContentLoaded', function () {
+  const storedTheme = localStorage.getItem('theme');
+
+  if (storedTheme !== null) {
+    setTheme(storedTheme);
+    console.log('Stored theme:', storedTheme);
+  } else {
+    console.log('Thene not found in localStorage');
+  }
 });
